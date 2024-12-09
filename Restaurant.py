36226b657f0a4455c4682997146fa52cdca1d3b9
haftomprogrammer <haftom.programmer@gmail.com>
2024-12-09 22:41:04 +0300

    'Clean_data'

--- Restaurant.py
+++ Restaurant.py
@@ -3,6 +3,13 @@ import numpy as np
 
 
 
+data_sales=pd.read_csv('sales_col.csv',sep=',',index_col='Date',parse_dates=['Date'])
+
+data_sales.head(5)
+print(data_sales.head(5))
+#data processing 
+print(data_sales['Qty'].sum())
+print('Null values in data set is',data_sales.isna().sum())
 
 
 #data processing 
