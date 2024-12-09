85c36f096a12a473d921b37ea9f698f7544f62f1
haftomprogrammer <haftom.programmer@gmail.com>
2024-12-09 23:01:35 +0300

    'visual'

--- Restaurant.py
+++ Restaurant.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pylab as plt
 
-
-
-data_sales=pd.read_csv('sales_col.csv',sep=',',index_col='Date',parse_dates=['Date'])
+#Data Pre Processing 
+data_sales=pd.read_csv('Project_Ref/sales_col.csv',sep=',',index_col='Date',parse_dates=['Date'])
 
 data_sales.head(5)
 print(data_sales.head(5))
@@ -12,4 +12,8 @@ print(data_sales['Qty'].sum())
 print('Null values in data set is',data_sales.isna().sum())
 
 
-#data processing 
+#Data Viualization 
+pivot_data=pd.pivot_table(data_sales,values='Qty',index='Date',
+                          columns='Date',aggfunc="sum")
+
+print(pivot_data)
